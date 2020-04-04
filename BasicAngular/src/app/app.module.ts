ee0f6ddb1155e5557c932681a3efe68ef41f8398
Rizan Zaky <rizan149@gmail.com>
2020-04-04 15:22:08 +0530

    add top bar

--- BasicAngular/src/app/app.module.ts
+++ BasicAngular/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: AppComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
